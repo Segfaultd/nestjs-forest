183bbb5bc4727cf86654715a9d55b91b85a1cff7
Segfault <5221072+Segfaultd@users.noreply.github.com>
2024-03-17 15:32:42 +0100

    add missing exports

--- dist/index.js
+++ dist/index.js
@@ -14,9 +14,20 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createSqlDataSource = void 0;
+exports.createElasticsearchDataSourceWithExistingClient = exports.createElasticsearchDataSource = void 0;
 __exportStar(require("./forest.module"), exports);
 __exportStar(require("./forest.service"), exports);
-var datasource_sql_1 = require("@forestadmin/datasource-sql");
-Object.defineProperty(exports, "createSqlDataSource", { enumerable: true, get: function () { return datasource_sql_1.createSqlDataSource; } });
+__exportStar(require("@forestadmin/agent"), exports);
+__exportStar(require("@forestadmin/datasource-customizer"), exports);
+__exportStar(require("@forestadmin/datasource-dummy"), exports);
+__exportStar(require("@forestadmin/datasource-live"), exports);
+__exportStar(require("@forestadmin/datasource-mongoose"), exports);
+__exportStar(require("@forestadmin/datasource-replica"), exports);
+__exportStar(require("@forestadmin/datasource-sequelize"), exports);
+__exportStar(require("@forestadmin/datasource-sql"), exports);
+__exportStar(require("@forestadmin/datasource-toolkit"), exports);
+var datasource_elasticsearch_1 = require("@forestadmin-experimental/datasource-elasticsearch");
+Object.defineProperty(exports, "createElasticsearchDataSource", { enumerable: true, get: function () { return datasource_elasticsearch_1.createElasticsearchDataSource; } });
+Object.defineProperty(exports, "createElasticsearchDataSourceWithExistingClient", { enumerable: true, get: function () { return datasource_elasticsearch_1.createElasticsearchDataSourceWithExistingClient; } });
+__exportStar(require("@forestadmin-experimental/datasource-hubspot"), exports);
 //# sourceMappingURL=index.js.map
--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,15 @@
 export * from './forest.module';
 export * from './forest.service';
 
-export { createSqlDataSource } from '@forestadmin/datasource-sql';
+export * from '@forestadmin/agent';
+export * from '@forestadmin/datasource-customizer';
+export * from '@forestadmin/datasource-dummy'
+export * from '@forestadmin/datasource-live';
+export * from '@forestadmin/datasource-mongoose';
+export * from '@forestadmin/datasource-replica';
+export * from '@forestadmin/datasource-sequelize';
+export * from '@forestadmin/datasource-sql';
+export * from '@forestadmin/datasource-toolkit';
+
+export { createElasticsearchDataSource, createElasticsearchDataSourceWithExistingClient } from '@forestadmin-experimental/datasource-elasticsearch';
+export * from '@forestadmin-experimental/datasource-hubspot';
