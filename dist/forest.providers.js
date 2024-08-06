d2fb2a2fe23ab17f0654edd9b8ff3ce6f7528b18
Segfault <5221072+Segfaultd@users.noreply.github.com>
2024-08-06 10:48:25 +0200

    Bump deps

--- dist/forest.providers.js
+++ dist/forest.providers.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createForestProviders = void 0;
+exports.createForestProviders = createForestProviders;
 const forest_constants_1 = require("./forest.constants");
 const forest_service_1 = require("./forest.service");
 function createForestProviders(options) {
@@ -9,5 +9,4 @@ function createForestProviders(options) {
         useValue: new forest_service_1.ForestService(options),
     };
 }
-exports.createForestProviders = createForestProviders;
 //# sourceMappingURL=forest.providers.js.map
